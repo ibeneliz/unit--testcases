135870201bccbe2b817009e87dd245eb7dbd39dd
ibeneliz <ibeneliz@gmail.com>
2024-04-26 10:56:40 +0530

    Unit test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('First text message renders successfully', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/first test/i);
+  expect(textElement).toBeInTheDocument();
 });
